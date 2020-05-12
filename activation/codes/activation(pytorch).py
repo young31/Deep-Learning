cfd13755c10ef60415931f48b2edc1595f9f9123
young31 <migael38317@gmail.com>
2020-05-12 15:26:50 +0900

    add gelu as torch.nn

--- activation/codes/activation(pytorch).py
+++ activation/codes/activation(pytorch).py
@@ -11,14 +11,6 @@ class Swish(nn.Module):
         return x * (torch.sigmoid(x))
 
 
-class GELU(nn.Module):
-    def __init__(self):
-        super(GELU, self).__init__()
-
-    def forward(self, x):
-        return 0.5*x*(1+torch.tanh(np.sqrt(2/np.pi)*(x+0.044715*F.pow(x, 3))))
-
-
 class Mish(nn.Module):
     def __init__(self):
         super(Mish, self).__init__()
